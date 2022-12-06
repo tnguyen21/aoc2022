270249c4dd8dce5c90219d1120659079facc85da
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2022-12-05 23:32:07 -0600

    add template to use for new days

---
+++ template/part1.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'test_input.txt')
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    for line in lines:
+        ...
+    return 0
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
